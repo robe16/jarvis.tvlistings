fe29e03c10a09af65aeebd07ef6bd48adedda5c8
Rob <robe16@hotmail.co.uk>
2018-04-08 12:17:45 +0100

    Initial commit of DevOps files

--- src/resources/global_resources/exposed_apis.py
+++ src/resources/global_resources/exposed_apis.py
@@ -1,3 +1,3 @@
 uri_config = '/config'
 uri_get_tvlistings_all = '/tvlistings/all'
-uri_get_tvlistings_channel = '/tvlistings/channel/<channame>'
+uri_get_tvlistings_channel = '/tvlistings/channel/<c>'
