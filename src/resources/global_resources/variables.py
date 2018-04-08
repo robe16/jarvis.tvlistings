7a20f4a49a8f2406fe8eb6ba61a7bdad52ab87b1
Rob <robe16@hotmail.co.uk>
2018-04-08 10:49:53 +0100

    Initial commit - resources

---
+++ src/resources/global_resources/variables.py
@@ -0,0 +1,10 @@
+serviceName = 'Jarvis: TV Listings'
+serviceType = 'tvlistings'
+
+service_header_clientid_label = 'jarvis.client-service'
+
+httpStatusSuccess = 200
+httpStatusBadrequest = 400
+httpStatusForbidden = 404
+httpStatusFailure = 420
+httpStatusServererror = 500
---
+++ src/resources/lang/enGB/logs.py
@@ -0,0 +1,6 @@
+logDescStartingService = 'Starting micro service'
+logDescPortListener = 'Port listener - {port}'
+logDescDeviceObjectCreation = 'Device object creation'
+
+logDesc_services_Broadcast = 'Broadcast of services'
+logDesc_Broadcast = 'Broadcasting message'
