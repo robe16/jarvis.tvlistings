8b5766d8c9f0e0d987a5555e70a869a1f9b67e51
Rob <robe16@hotmail.co.uk>
2018-06-15 16:44:53 +0100

    Updated to new code structure that has each API script in own module and and common request-based code in common_functions.

--- src/portlistener.py
+++ src/portlistener.py
@@ -1,19 +1,17 @@
 import threading
 
-from bottle import HTTPError
 from bottle import get
-from bottle import request, run, HTTPResponse
+from bottle import request, run
 
-from common_functions.urlencode import url_decode
-from common_functions.query_to_string import convert_query_to_string
 from config.config import get_cfg_port_listener
-from config.config import get_cfg_serviceid, get_cfg_name_long, get_cfg_name_short, get_cfg_groups, get_cfg_subservices
-from log.log import log_inbound, log_internal, log_outbound
-from resources.global_resources.exposed_apis import *
-from resources.global_resources.log_vars import logPass, logFail, logException
-from resources.global_resources.variables import *
+from log.log import log_internal
+from resources.global_resources.log_vars import logPass
 from resources.lang.enGB.logs import *
 from service.tvlistings import TVlistings
+
+from apis.uri_config import get_config
+from apis.uri_get_tvlistings_all import get_tvlistings_all
+from apis.uri_get_tvlistings_channel import get_tvlistings_channel
 
 
 def start_bottle(port_threads):
@@ -27,177 +25,20 @@ def start_bottle(port_threads):
     log_internal(logPass, logDescDeviceObjectCreation, description='success')
 
     ################################################################################################
-    # Enable cross domain scripting
+    # APIs
     ################################################################################################
 
-    def enable_cors(response):
-        response.headers['Access-Control-Allow-Origin'] = '*'
-        response.headers['Access-Control-Allow-Methods'] = 'GET'
-        response.headers['Access-Control-Allow-Headers'] = service_header_clientid_label
-        return response
+    @get('/config')
+    def api_get_config():
+        return get_config(request)
 
-    ################################################################################################
-    # Log arguments
-    ################################################################################################
+    @get('/tvlistings/all')
+    def api_get_tvlistings_all():
+        return get_tvlistings_all(request, _tvlistings)
 
-    def _get_log_args(request):
-        #
-        urlparts = request.urlparts
-        #
-        try:
-            client_ip = request.headers['X-Forwarded-For']
-        except:
-            client_ip = request['REMOTE_ADDR']
-        #
-        try:
-            server_ip = request.headers['X-Real-IP']
-        except:
-            server_ip = urlparts.hostname
-        #
-        try:
-            client_user = request.headers[service_header_clientid_label]
-        except:
-            client_user = request['REMOTE_ADDR']
-        #
-        server_request_query = convert_query_to_string(request.query) if request.query_string else '-'
-        server_request_body = request.body.read() if request.body.read()!='' else '-'
-        #
-        return {'client_ip': client_ip,
-                'client_user': client_user,
-                'server_ip': server_ip,
-                'server_thread_port': urlparts.port,
-                'server_method': request.method,
-                'server_request_uri': urlparts.path,
-                'server_request_query': server_request_query,
-                'server_request_body': server_request_body}
-
-    ################################################################################################
-    # Service info & Groups
-    ################################################################################################
-
-    @get(uri_config)
-    def get_config():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            data = {'service_id': get_cfg_serviceid(),
-                    'name_long': get_cfg_name_long(),
-                    'name_short': get_cfg_name_short(),
-                    'subservices': get_cfg_subservices(),
-                    'groups': get_cfg_groups()}
-            #
-            status = httpStatusSuccess
-            #
-            args['result'] = logPass
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            return HTTPResponse(body=data, status=status)
-            #
-        except Exception as e:
-            #
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
-
-    ################################################################################################
-    # TV Listings - all
-    ################################################################################################
-
-    @get(uri_get_tvlistings_all)
-    def get_tvlistings_all():
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            data = _tvlistings.get_listings_all()
-            #
-            if not bool(data):
-                status = httpStatusFailure
-                args['result'] = logFail
-            else:
-                status = httpStatusSuccess
-                args['result'] = logPass
-            #
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            if not isinstance(data, bool):
-                response.body = data
-            #
-            return response
-            #
-        except Exception as e:
-            #
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
-
-    ################################################################################################
-    # TV Listings - channel
-    ################################################################################################
-
-    @get(uri_get_tvlistings_channel)
-    def get_tvlistings_all(channame):
-        #
-        args = _get_log_args(request)
-        #
-        try:
-            #
-            data = _tvlistings.get_listings_channel(url_decode(channame))
-            #
-            if not bool(data):
-                status = httpStatusFailure
-                args['result'] = logFail
-            else:
-                status = httpStatusSuccess
-                args['result'] = logPass
-            #
-            args['http_response_code'] = status
-            args['description'] = '-'
-            log_inbound(**args)
-            #
-            response = HTTPResponse()
-            response.status = status
-            enable_cors(response)
-            #
-            if not isinstance(data, bool):
-                response.body = data
-            #
-            return response
-            #
-        except Exception as e:
-            #
-            status = httpStatusServererror
-            #
-            args['result'] = logException
-            args['http_response_code'] = status
-            args['description'] = '-'
-            args['exception'] = e
-            log_inbound(**args)
-            #
-            raise HTTPError(status)
+    @get('/tvlistings/channel/<channame>')
+    def api_get_tvlistings_channel(channame):
+        return get_tvlistings_channel(request, _tvlistings, channame)
 
     ################################################################################################
 
--- src/resources/global_resources/exposed_apis.py
+++
@@ -1,3 +0,0 @@
-uri_config = '/config'
-uri_get_tvlistings_all = '/tvlistings/all'
-uri_get_tvlistings_channel = '/tvlistings/channel/<channame>'
